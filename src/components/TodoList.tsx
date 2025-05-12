f6d314a2d8363cdd03431ba48775ad6d0dae6d89
Ronny Perez <ronnypdev@gmail.com>
2025-05-12 19:09:24 -0400

    Fixed user adding empty values bug and refactor the code

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,13 +1,13 @@
-'use client'
+'use client';
 
-import { useState } from "react";
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
 
-import CrossIcon from "./icons/CrossIcon";
-import OvalIcon from "./icons/OvalIcon";
-import PencilIcon from "./icons/PencilIcon";
+import CrossIcon from './icons/CrossIcon';
+import OvalIcon from './icons/OvalIcon';
+import PencilIcon from './icons/PencilIcon';
 
-import TodoListItem from "./TodoListItem";
+import TodoListItem from './TodoListItem';
 
 type TodoList = {
   id: string;
@@ -15,13 +15,13 @@ type TodoList = {
   completed: boolean;
   reveal: boolean;
   filterNames: string[];
-}
+};
 
 export default function TodoList() {
-  const [todoLisItem, setTodoListItem] = useState<string>("");
-  const [addListItems, setAddListItems] = useState<TodoList[]>([]);
+  const [todoLisItem, setTodoListItem] = useState<string>('');
+  const [listItems, setListItems] = useState<TodoList[]>([]);
   const [isActiveIndex, setIsActiveIndex] = useState<null | number>(null);
-  const [filter, setFilter] = useState<string>("All");
+  const [filter, setFilter] = useState<string>('All');
   const [itemChecked, setItemChecked] = useState<boolean>(true);
 
   function handleTodoItemChange(event: React.ChangeEvent<HTMLInputElement>) {
@@ -29,49 +29,55 @@ export default function TodoList() {
   }
 
   function addTodoItem() {
-    setAddListItems([
-      ...addListItems,
+    setListItems([
+      ...listItems,
       {
         id: nanoid(),
         listItem: todoLisItem,
         completed: false,
         reveal: false,
-        filterNames: ["All", "Active", "Completed"],
-      }
-    ])
-    setTodoListItem("");
+        filterNames: ['All', 'Active', 'Completed'],
+      },
+    ]);
+    setTodoListItem('');
   }
 
   function removeTodoItem(index: number) {
-    setAddListItems(prevAddListItems =>
-      prevAddListItems.filter(item =>
-        item.id !== addListItems[index].id
-      )
+    setListItems((prevListItems) =>
+      prevListItems.filter((item) => item.id !== listItems[index].id)
     );
   }
 
   function editTodoItem(itemId: string) {
-    setAddListItems(prevAddListItems =>
-      prevAddListItems.map(item =>
+    setListItems((prevListItems) =>
+      prevListItems.map((item) =>
         item.id === itemId ? { ...item, reveal: !item.reveal } : item
       )
     );
   }
 
-  function updateTodoItem(itemIndex: number, event: React.ChangeEvent<HTMLInputElement>) {
+  function updateTodoItem(
+    itemIndex: number,
+    event: React.ChangeEvent<HTMLInputElement>
+  ) {
     const { value } = event.target;
-    setAddListItems(prevAddListItems =>
-      prevAddListItems.map(item =>
-        item.id === addListItems[itemIndex].id ? {...item, listItem: value} : item
+    setListItems((prevListItems) =>
+      prevListItems.map((item) =>
+        item.id === listItems[itemIndex].id
+          ? { ...item, listItem: value }
+          : item
       )
-    )
+    );
   }
 
-  function checkCompleteItem(itemId: string, event: React.ChangeEvent<HTMLInputElement>) {
+  function checkCompleteItem(
+    itemId: string,
+    event: React.ChangeEvent<HTMLInputElement>
+  ) {
     const isChecked = event.target.checked;
-    setAddListItems((prevAddListItems) =>
-      prevAddListItems.map(item =>
-        item.id === itemId ? {...item, completed: isChecked} : item
+    setListItems((prevListItems) =>
+      prevListItems.map((item) =>
+        item.id === itemId ? { ...item, completed: isChecked } : item
       )
     );
   }
@@ -82,11 +88,12 @@ export default function TodoList() {
   }
 
   function uncheckItems() {
-    setItemChecked(false)
+    setItemChecked(false);
   }
 
   function submitTodoData(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    if (todoLisItem.trim() === '') return;
     addTodoItem();
   }
 
@@ -107,22 +114,27 @@ export default function TodoList() {
         </label>
         <div className="w-full h-full max-w-full bg-white rounded-[5px] shadow-(--light-box-shadow)">
           <div className="h-[85%] overflow-y-auto min-h-[auto]">
-            {addListItems.map((item, index) => (
+            {listItems.map((item, index) => (
               <>
-                {filter === "All" && (
-                  <div key={item.id}
-                    className={`p-6 ${index === 0 ? "border-t first:border-0" : "border-t border-t-light-grey"}
-                    flex justify-between items-center group/controls`}
-                  >
+                {filter === 'All' && (
+                  <div
+                    key={item.id}
+                    className={`p-6 ${
+                      index === 0
+                        ? 'border-t first:border-0'
+                        : 'border-t border-t-light-grey'
+                    }
+                    flex justify-between items-center group/controls`}>
                     <div className="todo-flex-col">
-                      <input className="cursor-pointer checkbox-round relative right-[11px] bottom-[2px]"
+                      <input
+                        className="cursor-pointer checkbox-round relative right-[11px] bottom-[2px]"
                         type="checkbox"
                         id={item.id}
                         checked={item.completed}
                         name={item.id}
                         onChange={(event) => {
                           checkCompleteItem(item.id, event);
-                          setItemChecked(!itemChecked)
+                          setItemChecked(!itemChecked);
                         }}
                       />
                       <TodoListItem
@@ -135,24 +147,33 @@ export default function TodoList() {
                       />
                     </div>
                     <div className="hidden group-hover/controls:flex group-hover/controls:justify-center group-hover/controls:items-center">
-                      {!item.completed && <PencilIcon
-                        fillColor="#494C6B"
-                        toggleOnClick={() => editTodoItem(item.id)}
-                        hoverState="hover:fill-midGrey cursor-pointer mr-2" />}
+                      {!item.completed && (
+                        <PencilIcon
+                          fillColor="#494C6B"
+                          toggleOnClick={() => editTodoItem(item.id)}
+                          hoverState="hover:fill-midGrey cursor-pointer mr-2"
+                        />
+                      )}
                       <CrossIcon
                         fillColor="#494C6B"
                         toggleOnClick={() => removeTodoItem(index)}
-                        hoverState="hover:fill-midGrey cursor-pointer mr-2" />
+                        hoverState="hover:fill-midGrey cursor-pointer mr-2"
+                      />
                     </div>
-                  </div>)
-                }
-                {filter === "Active" && !item.completed && (
-                  <div key={item.id}
-                    className={`p-6 ${index === 0 ? "border-t first:border-0" : "border-t border-t-light-grey"}
-                    flex justify-between items-center group/controls`}
-                  >
+                  </div>
+                )}
+                {filter === 'Active' && !item.completed && (
+                  <div
+                    key={item.id}
+                    className={`p-6 ${
+                      index === 0
+                        ? 'border-t first:border-0'
+                        : 'border-t border-t-light-grey'
+                    }
+                    flex justify-between items-center group/controls`}>
                     <div className="todo-flex-col">
-                      <input className="cursor-pointer checkbox-round relative right-[11px] bottom-[2px]"
+                      <input
+                        className="cursor-pointer checkbox-round relative right-[11px] bottom-[2px]"
                         type="checkbox"
                         id={item.id}
                         checked={item.completed}
@@ -169,24 +190,33 @@ export default function TodoList() {
                       />
                     </div>
                     <div className="hidden group-hover/controls:flex group-hover/controls:justify-center group-hover/controls:items-center">
-                      {!item.completed && <PencilIcon
-                        fillColor="#494C6B"
-                        toggleOnClick={() => editTodoItem(item.id)}
-                        hoverState="hover:fill-midGrey cursor-pointer mr-2" />}
+                      {!item.completed && (
+                        <PencilIcon
+                          fillColor="#494C6B"
+                          toggleOnClick={() => editTodoItem(item.id)}
+                          hoverState="hover:fill-midGrey cursor-pointer mr-2"
+                        />
+                      )}
                       <CrossIcon
                         fillColor="#494C6B"
                         toggleOnClick={() => removeTodoItem(index)}
-                        hoverState="hover:fill-midGrey cursor-pointer mr-2" />
+                        hoverState="hover:fill-midGrey cursor-pointer mr-2"
+                      />
                     </div>
-                  </div>)
-                }
-                {filter === "Completed" && item.completed && (
-                  <div key={item.id}
-                    className={`p-6 ${index === 0 ? "border-t first:border-0" : "border-t border-t-light-grey"}
-                    flex justify-between items-center group/controls`}
-                  >
+                  </div>
+                )}
+                {filter === 'Completed' && item.completed && (
+                  <div
+                    key={item.id}
+                    className={`p-6 ${
+                      index === 0
+                        ? 'border-t first:border-0'
+                        : 'border-t border-t-light-grey'
+                    }
+                    flex justify-between items-center group/controls`}>
                     <div className="todo-flex-col">
-                      <input className="cursor-pointer checkbox-round relative right-[11px] bottom-[2px]"
+                      <input
+                        className="cursor-pointer checkbox-round relative right-[11px] bottom-[2px]"
                         type="checkbox"
                         id={item.id}
                         checked={item.completed}
@@ -203,44 +233,63 @@ export default function TodoList() {
                       />
                     </div>
                     <div className="hidden group-hover/controls:flex group-hover/controls:justify-center group-hover/controls:items-center">
-                      {!item.completed && <PencilIcon
-                        fillColor="#494C6B"
-                        toggleOnClick={() => editTodoItem(item.id)}
-                        hoverState="hover:fill-midGrey cursor-pointer mr-2" />}
+                      {!item.completed && (
+                        <PencilIcon
+                          fillColor="#494C6B"
+                          toggleOnClick={() => editTodoItem(item.id)}
+                          hoverState="hover:fill-midGrey cursor-pointer mr-2"
+                        />
+                      )}
                       <CrossIcon
                         fillColor="#494C6B"
                         toggleOnClick={() => removeTodoItem(index)}
-                        hoverState="hover:fill-midGrey cursor-pointer mr-2" />
+                        hoverState="hover:fill-midGrey cursor-pointer mr-2"
+                      />
                     </div>
-                  </div>)
-                }
+                  </div>
+                )}
               </>
             ))}
           </div>
 
           <div className="controls border-t border-t-light-grey h-[15%] flex justify-between items-center px-[14px]">
-            <p className="text-shade-grey"><span>{addListItems.length}</span> items left</p>
-              <ul className="flex justify-between items-center">
-                {addListItems.length > 0 && addListItems[0].filterNames ? (
-                  addListItems[0].filterNames.map((filterItem, index) => (
-                    <li
-                      key={index}
-                      className={`ml-4 cursor-pointer text-primary-blue ${index === isActiveIndex ? "text-primary-blue" : "text-shade-grey"}`}
-                      onClick={() => toggleActive(index, filterItem)}
-                    >{filterItem}</li>
-                  ))
-                ) : (
-                  <>
-                    <li className="ml-4 cursor-pointer text-primary-blue">All</li>
-                    <li className="ml-4 cursor-pointer text-shade-grey">Active</li>
-                    <li className="ml-4 cursor-pointer text-shade-grey">Completed</li>
-                  </>
-                )}
-              </ul>
-            <p className="text-shade-grey cursor-pointer" onClick={() => uncheckItems()}>Clear Completed</p>
+            <p className="text-shade-grey">
+              <span>{listItems.length}</span> items left
+            </p>
+            <ul className="flex justify-between items-center">
+              {listItems.length > 0 && listItems[0].filterNames ? (
+                listItems[0].filterNames.map((filterItem, index) => (
+                  <li
+                    key={index}
+                    className={`ml-4 cursor-pointer text-primary-blue ${
+                      index === isActiveIndex
+                        ? 'text-primary-blue'
+                        : 'text-shade-grey'
+                    }`}
+                    onClick={() => toggleActive(index, filterItem)}>
+                    {filterItem}
+                  </li>
+                ))
+              ) : (
+                <>
+                  <li className="ml-4 cursor-pointer text-primary-blue">All</li>
+                  <li className="ml-4 cursor-pointer text-shade-grey">
+                    Active
+                  </li>
+                  <li className="ml-4 cursor-pointer text-shade-grey">
+                    Completed
+                  </li>
+                </>
+              )}
+            </ul>
+            <p
+              className="text-shade-grey cursor-pointer"
+              onClick={() => uncheckItems()}>
+              Clear Completed
+            </p>
           </div>
         </div>
       </form>
     </>
-  )
+  );
 }
