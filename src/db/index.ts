cf7c1d0f531af9e4073c1927171d06e05e9a2159
Ronny Perez <ronnypdev@gmail.com>
2025-05-14 20:48:59 -0400

    DB connections is broken

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,6 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
+import { eq } from 'drizzle-orm';
+import { listItemTable } from './schemas/schema';
 import postgres from 'postgres';
 
 async function main() {
@@ -7,5 +9,25 @@ async function main() {
   }
   const client = postgres(process.env.DATABASE_URL);
   const db = drizzle(client);
+
+  const todoItem: typeof listItemTable.$inferInsert = {
+    updatedItem: 'test item product',
+    completed: false,
+  };
+
+  await db.insert(listItemTable).values(todoItem);
+  console.log('Inserted item');
+
+  const todoItemFromDb = await db.select().from(listItemTable);
+  console.log('this is the current item', todoItemFromDb);
+
+  await db
+    .update(listItemTable)
+    .set({ updatedItem: 'remember to buy milk' })
+    .where(eq(listItemTable.id, 1));
+  console.log('updated item');
+
+  await db.delete(listItemTable).where(eq(listItemTable.id, 1));
+  console.log('deleted item');
 }
 main();
