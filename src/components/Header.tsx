d3b52b9fb58d64d3961d6387f19640ba6b8b3044
Ronny Perez <ronnypdev@gmail.com>
2025-06-10 17:04:50 -0400

    Implemented dark mode on header and fixed icon props

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,19 @@
+import { HeaderProps } from '@/types';
 import MoonIcon from './icons/MoonIcon';
+import IconSun from './icons/iconSun';
 
-export default function Header() {
+export default function Header({
+  toggleDarkMode,
+  toggleDarkModeClickHandler,
+}: HeaderProps) {
   return (
     <>
-      <header className="bg-(image:--hero-mobile-light) md:bg-(image:--hero-desktop-light) h-[330px] p-5 md:p-0 bg-cover bg-center bg-no-repeat">
+      <header
+        className={`${
+          toggleDarkMode
+            ? 'bg-(image:--hero-mobile-light) md:bg-(image:--hero-desktop-light)'
+            : 'bg-(image:--hero-mobile-dark) md:bg-(image:--hero-desktop-dark)'
+        } h-[330px] p-5 md:p-0 bg-cover bg-center bg-no-repeat`}>
         <div className="max-container h-full">
           <div className="flex justify-center items-center h-full">
             <div className="w-[540px] my-0 mx-auto">
@@ -11,10 +21,21 @@ export default function Header() {
                 <h1 className="text-white text-[40px] leading-normal uppercase font-bold style not-italic tracking-[15px]">
                   todo
                 </h1>
-                <MoonIcon
-                  fillColor="#FFFFFF"
-                  hoverState="hover:fill-white cursor-pointer"
-                />
+                <div
+                  className="header-icon-group"
+                  onClick={toggleDarkModeClickHandler}>
+                  {toggleDarkMode ? (
+                    <MoonIcon
+                      fillColor="#FFFFFF"
+                      hoverState="hover:fill-white cursor-pointer"
+                    />
+                  ) : (
+                    <IconSun
+                      fillColor="#FFFFFF"
+                      hoverState="hover:fill-white cursor-pointer"
+                    />
+                  )}
+                </div>
               </div>
             </div>
           </div>
--- src/components/icons/iconCheck.tsx
+++ src/components/icons/iconCheck.tsx
@@ -1,20 +1,28 @@
-type iconProps = {
+type IconProps = {
   fillColor: string;
   stroke?: string;
   hoverState: string;
-}
-
+};
 
 export default function iconCheck({
   fillColor,
   stroke,
   hoverState,
-}: iconProps) {
+}: IconProps) {
   return (
     <>
-      <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9" className={hoverState}>
-        <path fill={fillColor} stroke={stroke} stroke-width="2" d="M1 4.304L3.696 7l6-6" />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="11"
+        height="9"
+        className={hoverState}>
+        <path
+          fill={fillColor}
+          stroke={stroke}
+          stroke-width="2"
+          d="M1 4.304L3.696 7l6-6"
+        />
       </svg>
     </>
-  )
+  );
 }
